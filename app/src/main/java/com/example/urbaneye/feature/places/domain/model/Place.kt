6a68111090e8bf7f27083178598a19620c81c071
Kelvin <kelvindelima710@gmail.com>
2026-07-09 23:20:47 -0300

    feat: create Place model and repository

--- app/src/main/java/com/example/urbaneye/feature/places/domain/model/Place.kt
+++ app/src/main/java/com/example/urbaneye/feature/places/domain/model/Place.kt
@@ -1,14 +1,15 @@
 package com.example.urbaneye.feature.places.domain.model
 
-class Place(
+data class Place(
     val id: String,
     val name: String,
     val type: String,
+    val address: String,
     val latitude: Double,
     val longitude: Double,
-) {
-
-    override fun toString(): String {
-        return super.toString()
-    }
-}
+    val description: String?,
+    val rating: Double?,
+    val photoUrl: String?,
+    val contactPhone: String?,
+    val isOpenNow: Boolean?
+)
--- app/src/main/java/com/example/urbaneye/feature/places/domain/repository/PlacesRepository.kt
+++ app/src/main/java/com/example/urbaneye/feature/places/domain/repository/PlacesRepository.kt
@@ -1,1 +1,10 @@
 package com.example.urbaneye.feature.places.domain.repository
+import com.example.urbaneye.feature.places.domain.model.Place
+
+interface PlacesRepository {
+    // suspend define q uma func é assíncrona
+    suspend fun getNearbyPlaces(latitude: Double, longitude: Double, radius: Int): Result<List<Place>>
+    suspend fun getPlaceDetails(placeId: String): Result<Place>
+}
+
+
