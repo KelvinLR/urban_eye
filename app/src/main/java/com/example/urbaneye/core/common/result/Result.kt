8210dd7355c9b0bdaf7d9461e9d6880711bcbcd3
Kelvin <kelvindelima710@gmail.com>
2026-07-09 22:40:16 -0300

    feat: implement Result class to handle errors and sucess states

--- app/src/main/java/com/example/urbaneye/core/common/result/Result.kt
+++ app/src/main/java/com/example/urbaneye/core/common/result/Result.kt
@@ -1,1 +1,11 @@
 package com.example.urbaneye.core.common.result
+
+// Result.kt
+// A classe criada é responsável por representar os resultados
+// das operações e facilitar a sua gestão, evitando q eu dê throw em excecoes
+
+sealed class Result<out T> {
+    data class Success<T>(val data: T) : Result<T>()
+    data class Error(val exception: Throwable) : Result<Nothing>()
+    data object Loading : Result<Nothing>()
+}
