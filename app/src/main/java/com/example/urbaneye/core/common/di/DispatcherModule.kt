d138f7676c2ba970fb15649b6220230d2bd18c6b
Kelvin <kelvindelima710@gmail.com>
2026-07-22 00:42:03 -0300

    feat: create DispatcherModule

--- app/src/main/java/com/example/urbaneye/core/common/di/DispatcherModule.kt
+++ app/src/main/java/com/example/urbaneye/core/common/di/DispatcherModule.kt
@@ -1,9 +1,18 @@
 package com.example.urbaneye.core.common.di
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Qualifier
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
+// criando as annotations para cada dispatcher
 @Qualifier
+// esse tipo da annotation vai definir por qnt tempo ela
+// vai sobreviver no codigo, tipo aq ela vai ser compilada
+// mas vai evitar q ela gaste memoria em Runtime
+// isso eh massa pq evita crashes por falta de memoria no celular
 @Retention(AnnotationRetention.BINARY)
 annotation class MainDispatcher
 
@@ -15,3 +24,19 @@ annotation class IODispatcher
 @Retention(AnnotationRetention.BINARY)
 annotation class DefaultDispatcher
 
+// fazendo com object e obedecendo o padrão Singleton p evitar multiplas instâncias
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+    @Provides
+    @MainDispatcher
+    fun providesMainDispatcher() : CoroutineDispatcher = Dispatchers.Main
+
+    @Provides
+    @IODispatcher
+    fun providesIODispatcher() : CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @DefaultDispatcher
+    fun providesDefaultDispatcher() : CoroutineDispatcher = Dispatchers.Default
+}
