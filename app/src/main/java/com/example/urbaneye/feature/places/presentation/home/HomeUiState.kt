90ce2aa2787d153a2218a11a25fda80197e2cbbb
Kelvin <kelvindelima710@gmail.com>
2026-08-21 23:17:43 -0300

    feat: creating homeuistate class

--- app/src/main/java/com/example/urbaneye/feature/places/presentation/home/HomeUiState.kt
+++ app/src/main/java/com/example/urbaneye/feature/places/presentation/home/HomeUiState.kt
@@ -1,1 +1,9 @@
 package com.example.urbaneye.feature.places.presentation.home
+
+import com.example.urbaneye.feature.places.domain.model.Place
+
+data class HomeUiState(
+    val isLoading: Boolean = false,
+    val places: List<Place> = emptyList(),
+    val errorMessage: String? = null
+)
