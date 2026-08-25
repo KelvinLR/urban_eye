d0412d0c56b5fe641637aa94c36d3a9829d9bda6
Kelvin <kelvindelima710@gmail.com>
2026-08-24 23:06:13 -0300

    feat: starting adjustements at SearchBar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
 
     // Hilt
     implementation(libs.hilt.android)
+    implementation(libs.androidx.compose.material.core)
     ksp(libs.hilt.android.compiler)
     implementation(libs.hilt.navigation.compose)
 
--- app/src/main/java/com/example/urbaneye/feature/places/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/urbaneye/feature/places/presentation/home/HomeScreen.kt
@@ -1,19 +1,24 @@
 package com.example.urbaneye.feature.places.presentation.home
-import android.widget.ListView
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.urbaneye.feature.places.presentation.components.PlaceCard
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 
@@ -34,10 +39,21 @@ fun HomeScreen() {
             query = "",
             onQueryChange = { },
             onSearch = { },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "Search"
+                )
+            },
             active = false,
             onActiveChange = { },
             placeholder = { Text("Onde vamos hoje?") }
-        ) { }
+        ) {
+            Modifier.size(
+                width = 358.dp,
+                height = 192f.dp,
+            ).background(Color(0xFFBFCABA))
+        }
 
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(24.dp),
