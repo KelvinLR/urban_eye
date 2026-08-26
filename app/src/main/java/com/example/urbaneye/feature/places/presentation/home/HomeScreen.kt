82397c5803eb94fad5bc5a08070835563c2938b4
Kelvin <kelvindelima710@gmail.com>
2026-08-25 23:55:11 -0300

    fix: adjust text elements arrangement

--- app/src/main/java/com/example/urbaneye/feature/places/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/urbaneye/feature/places/presentation/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.urbaneye.feature.places.presentation.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -10,13 +11,18 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.urbaneye.feature.places.presentation.components.PlaceCard
 
 
@@ -25,14 +31,26 @@ import com.example.urbaneye.feature.places.presentation.components.PlaceCard
 @Composable
 fun HomeScreen() {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color(0xFFFFFFFF))
+                .padding(16.dp)
     ) {
         TopAppBar(
-            title = { Text(text = "UrbanEye") }
-
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color(0xFFFFFFFF),
+                titleContentColor = Color(0xFF0D631B),
+            ),
+            title = {
+                Text(
+                    text = "UrbanEye",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight(800)
+                )
+            }
         )
 
-        Text(text = "Bem-vindo!")
+        Text(text = "Bem-vindo!", fontSize = 22.sp, fontWeight = FontWeight(500))
         Text("Para onde vamos hoje?")
 
         SearchBar (
@@ -57,7 +75,6 @@ fun HomeScreen() {
 
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(24.dp),
-            contentPadding = PaddingValues(all = 16.dp)
         ) {
             items(3) {
                 PlaceCard(
