422d5fef3823cfbebb8f25d20f0d669ba24d0593
Kelvin <kelvindelima710@gmail.com>
2026-08-28 19:24:25 -0300

    feat: create PlaceGenreSelector and PlaceGenreSelectorSlider composable components

--- app/src/main/java/com/example/urbaneye/core/designsystem/component/PlaceGenreSelector.kt
+++ app/src/main/java/com/example/urbaneye/core/designsystem/component/PlaceGenreSelector.kt
@@ -1,2 +1,31 @@
 package com.example.urbaneye.core.designsystem.component
 
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun PlaceGenreSelector(
+    label: String,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    ) {
+        FilterChip(
+            colors = FilterChipDefaults.filterChipColors(
+                containerColor = Color(0xFFE5E1E7),
+                labelColor = Color(0xFF40493D),
+
+                selectedContainerColor = Color(0xFF2E7D32),
+                selectedLabelColor = Color(0xFFCBFFC2),
+            ),
+            shape = CircleShape,
+            border = null,
+            selected = isSelected,
+            onClick = onClick,
+            label = { Text(label) }
+
+        )
+}
--- app/src/main/java/com/example/urbaneye/core/designsystem/component/PlaceGenreSelectorSlider.kt
+++ app/src/main/java/com/example/urbaneye/core/designsystem/component/PlaceGenreSelectorSlider.kt
@@ -1,21 +1,27 @@
 package com.example.urbaneye.core.designsystem.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun PlaceGenreSelectorSlider() {
-    LazyRow() { }
+fun PlaceGenreSelectorSlider(labels: List<String>) {
+    var selectedIndex by remember { mutableStateOf(labels.firstOrNull() ?: "") }
+
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(12.5.dp)
+    ) {
+        items(labels) { label ->
+            PlaceGenreSelector(
+                label = label,
+                isSelected = (label == selectedIndex),
+                onClick = { selectedIndex = label }
+            )
+        }
+    }
 }
 
-@Composable
-fun PlaceGenreSelector(label: String, isSelected: Boolean) {
-    val backgroudColor = if (isSelected) Color(0xFF0D631B) else Color(0xFFBFCABA)
-    val textColor = if (isSelected) Color(0xFFFFFFFF) else Color(0xFF000000)
-    val borderColor = if (isSelected) Color(0xFF0D631B) else Color(0xFFBFCABA)
 
-    FilterChip(selected = isSelected, onClick = { !isSelected }, label = { Text(label) })
-}
