a4d305e0a4b1837ff7828beea1e1a0d5e23e0fd7
Kelvin <kelvindelima710@gmail.com>
2026-08-28 15:37:46 -0300

    feat: create PlacesApiService interface with only name and id requests

--- app/src/main/java/com/example/urbaneye/feature/places/data/remote/api/PlacesApiService.kt
+++ app/src/main/java/com/example/urbaneye/feature/places/data/remote/api/PlacesApiService.kt
@@ -1,1 +1,16 @@
 package com.example.urbaneye.feature.places.data.remote.api
+
+import com.example.urbaneye.BuildConfig
+import kotlinx.serialization.json.JsonObject
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface PlacesApiService {
+    @POST("v1/places:searchNearby")
+    suspend fun searchNearby(
+        @Header("X-Goog-Api-Key") apiKey: String = BuildConfig.PLACES_API_KEY,
+        @Header("X-Goog-FieldMask") fieldMask: String = "places.id,places.displayName",
+        @Body requestBody: JsonObject
+    ): JsonObject
+}
