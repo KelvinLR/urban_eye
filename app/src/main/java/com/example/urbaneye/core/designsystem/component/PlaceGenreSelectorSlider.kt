ed23b491a74eb9b8dcaa939d7e924194b4fed132
Kelvin <kelvindelima710@gmail.com>
2026-08-29 22:27:54 -0300

    fix: adjust inner padding

--- app/src/main/java/com/example/urbaneye/core/designsystem/component/PlaceGenreSelectorSlider.kt
+++ app/src/main/java/com/example/urbaneye/core/designsystem/component/PlaceGenreSelectorSlider.kt
@@ -1,10 +1,12 @@
 package com.example.urbaneye.core.designsystem.component
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -12,6 +14,7 @@ fun PlaceGenreSelectorSlider(labels: List<String>) {
     var selectedIndex by remember { mutableStateOf(labels.firstOrNull() ?: "") }
 
     LazyRow(
+        modifier = Modifier.padding(vertical = 24.dp),
         horizontalArrangement = Arrangement.spacedBy(12.5.dp)
     ) {
         items(labels) { label ->
