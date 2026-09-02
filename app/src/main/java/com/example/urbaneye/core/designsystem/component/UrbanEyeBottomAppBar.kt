324bb57a8a428e121944f430481b2ef116ae2b0f
Kelvin <kelvindelima710@gmail.com>
2026-09-01 23:31:17 -0300

    feat: creating UrbanEyeBottomAppBar

--- app/src/main/java/com/example/urbaneye/core/designsystem/component/UrbanEyeBottomAppBar.kt
+++ app/src/main/java/com/example/urbaneye/core/designsystem/component/UrbanEyeBottomAppBar.kt
@@ -1,2 +1,55 @@
 package com.example.urbaneye.core.designsystem.component
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun UrbanEyeBottomAppBar() {
+    NavigationBar {
+        NavigationBarItem(
+            icon = { Icon(imageVector = Icons.Filled.Home, contentDescription = "Home") },
+            selected = true,
+            onClick = {},
+            label = {
+                Text("Home")
+            }
+        )
+        NavigationBarItem(
+            icon = { Icon(imageVector = Icons.Filled.Search, contentDescription = "Buscar") },
+            selected = false,
+            onClick = {},
+            label = {
+                Text("Buscar")
+            }
+        )
+        NavigationBarItem(
+            icon = { Icon(imageVector = Icons.Filled.Favorite, contentDescription = "Favoritos") },
+            selected = false,
+            onClick = {},
+            label = {
+                Text("Favoritos")
+            }
+        )
+        /*IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Filled.Home, contentDescription = "Home")
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Filled.Favorite, contentDescription = "Search")
+        }*/
+    }
+
+
+}
