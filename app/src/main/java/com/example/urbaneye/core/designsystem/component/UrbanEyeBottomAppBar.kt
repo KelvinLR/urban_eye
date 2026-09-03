a147e42ff3055b0f31c568f637f80bce954a8e9d
Kelvin <kelvindelima710@gmail.com>
2026-09-02 23:42:48 -0300

    refactor: creating UrbanEyeNavigationItem composable component and using in UrbanEyeBottomAppBar

--- app/src/main/java/com/example/urbaneye/core/designsystem/component/UrbanEyeBottomAppBar.kt
+++ app/src/main/java/com/example/urbaneye/core/designsystem/component/UrbanEyeBottomAppBar.kt
@@ -1,7 +1,11 @@
 package com.example.urbaneye.core.designsystem.component
 
+import android.R
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.materialIcon
@@ -10,46 +14,51 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun UrbanEyeBottomAppBar() {
     NavigationBar {
-        NavigationBarItem(
+        UrbanEyeNavigationItem(
             icon = { Icon(imageVector = Icons.Filled.Home, contentDescription = "Home") },
-            selected = true,
-            onClick = {},
-            label = {
-                Text("Home")
-            }
+            label = { Text("Home") },
+            isSelected = true,
+            onClick = {}
         )
-        NavigationBarItem(
+        UrbanEyeNavigationItem(
             icon = { Icon(imageVector = Icons.Filled.Search, contentDescription = "Buscar") },
-            selected = false,
-            onClick = {},
-            label = {
-                Text("Buscar")
-            }
+            label = { Text("Buscar") },
+            isSelected = false,
+            onClick = {}
         )
-        NavigationBarItem(
-            icon = { Icon(imageVector = Icons.Filled.Favorite, contentDescription = "Favoritos") },
-            selected = false,
-            onClick = {},
-            label = {
-                Text("Favoritos")
-            }
+        UrbanEyeNavigationItem(
+            icon = { Icon(imageVector = Icons.Filled.FavoriteBorder, contentDescription = "Favoritos") },
+            label = { Text("Favoritos") },
+            isSelected = false,
+            onClick = {}
         )
-        /*IconButton(onClick = { /*TODO*/ }) {
-            Icon(imageVector = Icons.Filled.Home, contentDescription = "Home")
-        }
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
-        }
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(imageVector = Icons.Filled.Favorite, contentDescription = "Search")
-        }*/
     }
+}
 
-
+@Composable
+fun RowScope.UrbanEyeNavigationItem(
+    icon: @Composable () -> Unit,
+    label: @Composable () -> Unit,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+    NavigationBarItem(
+        icon = icon,
+        selected = isSelected,
+        colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = Color(0xFFFFFFFF),
+            selectedTextColor = Color(0xFF0D631B),
+            indicatorColor = Color(0xFF0D631B)
+        ),
+        onClick = onClick,
+        label = label
+    )
 }
