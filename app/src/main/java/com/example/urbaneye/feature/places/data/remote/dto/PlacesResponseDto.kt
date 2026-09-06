724fefdcf45e2aecc892db6792a1ac4adb94863c
Kelvin <kelvindelima710@gmail.com>
2026-09-05 23:15:59 -0300

    feat: creating PlacesResponseDTO to start Places API integration

--- app/src/main/java/com/example/urbaneye/feature/places/data/remote/dto/PlacesResponseDto.kt
+++ app/src/main/java/com/example/urbaneye/feature/places/data/remote/dto/PlacesResponseDto.kt
@@ -1,1 +1,29 @@
 package com.example.urbaneye.feature.places.data.remote.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PlacesResponseDto(
+    val places: List<PlaceDto>? = null
+)
+
+@Serializable
+data class PlaceDto(
+    val id: String? = null,
+    val displayName: DisplayNameDto? = null,
+    val formattedAddress: String? = null,
+    val primaryType: String? = null,
+    val rating: Double? = null,
+    val location: LocationDto? = null
+)
+
+@Serializable
+data class DisplayNameDto(
+    val text: String? = null
+)
+
+@Serializable
+data class LocationDto(
+    val latitude: Double? = null,
+    val longitude: Double? = null
+)
