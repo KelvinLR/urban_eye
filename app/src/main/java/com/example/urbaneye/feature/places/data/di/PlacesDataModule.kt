83b35cc325c47535ce01482f826849a6e1b4cead
Kelvin <kelvindelima710@gmail.com>
2026-09-07 16:57:26 -0300

    feat: creating PlacesDataModule

--- app/src/main/java/com/example/urbaneye/feature/places/data/di/PlacesDataModule.kt
+++ app/src/main/java/com/example/urbaneye/feature/places/data/di/PlacesDataModule.kt
@@ -1,1 +1,31 @@
 package com.example.urbaneye.feature.places.data.di
+
+import com.example.urbaneye.feature.places.data.remote.api.PlacesApiService
+import com.example.urbaneye.feature.places.data.repository.PlacesRepositoryImpl
+import com.example.urbaneye.feature.places.domain.repository.PlacesRepository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class PlacesDataModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindPlacesRepository(
+        placesRepositoryImpl: PlacesRepositoryImpl
+    ): PlacesRepository
+
+    companion object {
+        @Provides
+        @Singleton
+        fun providePlacesApiService(retrofit: Retrofit): PlacesApiService {
+            return retrofit.create(PlacesApiService::class.java)
+        }
+    }
+}
